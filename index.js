8a406322a70f896833e2cda7e86c1dda397abd0b
Maurice Williams <morficus@gmail.com>
2014-05-02 00:46:26 -0400

    "templetizing" the output file and adding the ability to specify your own template

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 var packageFile = require('./package.json'),
-    fs = require('fs');
+    fs = require('fs'),
+    _ = require('underscore'),
+    ejs = require('ejs');
 
-var versionFile = module.exports = function(filePath){
+var versionFile = module.exports = function(configObject){
     var currentTime = (new Date()),
         name = packageFile.name,
         version = packageFile.version,
         fileContent;
 
-    filePath = filePath || 'version.txt';
+    var defaultConfig = {
+        pathToOutputFile: './version.txt',
+        pathToTemplate: 'version.ejs',
+        templateString: '',
+        extras: {}
+    };
 
-    fileContent = name + ' v' + version + '\n';
-    fileContent += 'Build date: ' + currentTime;
+    //set default config data
+    configObject = configObject || {};
+    configObject = _.defaults(configObject, defaultConfig);
 
-    fs.writeFile('version.txt', fileContent, {flag: 'w'});
+    // If we are given a template string in the config, then use it directly.
+    // But if we get a file path, fetch the content then use it.
+    if(configObject.templateString){
+        writeFile(configObject.templateString);
+    }else{
+        fs.readFile(configObject.pathToTemplate, {encoding: 'utf8'}, function(error, content){
+
+            if(error){
+                throw error;
+                return;
+            }
+
+            writeFile(content);
+        });
+    }
+
+    /**
+     * Renders the template and writes the version file to the file system.
+     * @param templateContent
+     */
+    var writeFile = function(templateContent){
+        fileContent = ejs.render(templateContent, {'name': name, 'version': version, 'time': currentTime, extras: configObject.exports});
+        console.log(fileContent);
+        fs.writeFile(configObject.pathToOutputFile, fileContent, {flag: 'w'});
+    }
 
 };
+
+versionFile();
